5907e28de070f1bb5eefe9d329867390dfe26fa4
MingDong She <scg130@163.com>
2022-05-03 18:17:14 +0800

    a

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	s := gin.Default()
 	s.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, "ok1 !!!")
+		ctx.JSON(http.StatusOK, "ok1!!!")
 	})
 	fmt.Println("7")
 	log.Fatal(s.Run())
